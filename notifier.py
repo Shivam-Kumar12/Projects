8ba019ccf25bebb7665dd013f64c55c0e8a657a1
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2023-02-04 22:52:25 +0530

    Add files via upload

---
+++ notifier.py
@@ -0,0 +1,9 @@
+import os
+from turtle import title
+ from plyer import notification
+ notification.notify(   
+    title="hello guys",
+    message="do not touch my pc",
+    app_icon=None,
+    timeout=10,
+)
