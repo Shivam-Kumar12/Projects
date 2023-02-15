7320d78d12e60c33b0d5e8f9f937e1885f22820f
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2023-02-15 18:40:30 +0530

    Add files via upload

---
+++ generatepassword.py
@@ -0,0 +1,28 @@
+l=["harry","larry","jarry","karry"]
+print(len(l))
+
+import pyautogui
+import time
+
+message=" I love U  "
+n=5
+
+
+# print('''I love you I love U  
+
+
+
+
+
+
+# ''')
+time.sleep(2)
+
+
+for i in range(n):
+    pyautogui.typewrite(message +"\n") 
+ 
+
+
+
+
