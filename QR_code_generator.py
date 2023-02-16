008bdfcde8101a2204bfc9b5cf1d318276213c28
Shivam Kumar <109784057+Shivam-Kumar12@users.noreply.github.com>
2023-02-16 21:40:56 +0530

    Update QR_code_generator.py

--- QR_code_generator.py
+++ QR_code_generator.py
@@ -2,11 +2,14 @@ import qrcode
 
 qr=qrcode.QRCode(
     version=10,
+# There are 40 version of QR_code to implement
     box_size=10,
     border=5
 )
 Data="https://github.com/Shivam-Kumar12"
+# In the above code we can keep the link to make QR code
 qr.add_data(Data)
 qr.make(fit=True)
 img=qr.make_image(fill="black",back_color="white")
+# In the below form the Qr code will be saved in that location of python File
 img.save("text.png")
